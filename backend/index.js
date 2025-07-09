fedb4720614d0b2d328f2ab9bab95f6560669419
Riya Maurya <146939873+Riyamaurya123@users.noreply.github.com>
2025-07-09 12:32:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,6 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 const corsOption = {
-    origin:"http://localhost:5173/",
     credentials:true
 }
 app.use(cors(corsOption))
